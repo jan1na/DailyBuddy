82a01516fb5c01de7af81a427d502cd6863604da
Robert Konrad <git2016@robdangero.us>
2020-03-21 05:55:02 +0100

    Pick dates

--- lib/fromto.dart
+++ lib/fromto.dart
@@ -7,6 +7,9 @@ class FromTo extends StatelessWidget {
   final String title;
   final ValueChanged<Widget> screenChanged;
 
+  DateTime from;
+  DateTime to;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,8 +21,49 @@ class FromTo extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Von wann bis wann',
+              'Von wann bis wann soll die Quarantäne gehen?',
               style: Theme.of(context).textTheme.display1,
+            ),
+            RaisedButton(
+              onPressed: () async {
+                  final DateTime picked = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2015, 8),
+                      lastDate: DateTime(2101));
+                  if (picked != null) {
+                    from = picked;
+                  }
+              },
+              child: Text(
+                'Beginn',
+                style: TextStyle(fontSize: 20)
+              ),
+            ),
+            RaisedButton(
+              onPressed: () async {
+                final DateTime picked = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2015, 8),
+                      lastDate: DateTime(2101));
+                  if (picked != null) {
+                    to = picked;
+                  }
+              },
+              child: Text(
+                'Ende',
+                style: TextStyle(fontSize: 20)
+              ),
+            ),
+            RaisedButton(
+              onPressed: () {
+                
+              },
+              child: Text(
+                'Weiter',
+                style: TextStyle(fontSize: 20)
+              ),
             ),
           ],
         ),
