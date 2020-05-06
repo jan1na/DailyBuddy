f3ba70591454e176e4bb23fe34e680c563080aad
wcrb <mailAthonHack@protonmail.com>
2020-05-06 23:37:01 +0200

    Code für EmergencyContactsPage in branch development übenehmen

--- lib/screens/emergency_contacts.dart
+++ lib/screens/emergency_contacts.dart
@@ -1,10 +1,140 @@
 import 'package:flutter/material.dart';
+import 'package:daily_buddy_app/themes.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class EmergencyContactsPage extends StatelessWidget {
+class EmergencyContactsPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _EmergencyContactsState();
+}
+
+class _EmergencyContactsState extends State<EmergencyContactsPage> {
+  Column _emergencyContacts = createEmergencyContacts('MyEmergencyContacts');
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("emergency_contacts.dart"),
+    final widthScreen = MediaQuery.of(context).size.width;
+    final heightScreen = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Notfallkontakte'),
+      ),
+      body: Center(
+        child: Container(
+            padding: EdgeInsets.only(top: 40.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                            padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 30.0),
+                            width: widthScreen / 3,
+                            height: heightScreen / 6,
+                            child: RaisedButton(
+                                color: defaultTheme.accentColor,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius:
+                                    BorderRadius.all(Radius.circular(18))),
+                                child: Text('Meine \nKontakte'),
+                                onPressed: () => setState(() {
+                                  _emergencyContacts =
+                                      createEmergencyContacts(
+                                          'MyEmergencyContacts');
+                                }))),
+                        Container(
+                            padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 30.0),
+                            width: widthScreen / 3,
+                            height: heightScreen / 6,
+                            child: RaisedButton(
+                                color: defaultTheme.accentColor,
+                                shape: RoundedRectangleBorder(
+                                    borderRadius:
+                                    BorderRadius.all(Radius.circular(18))),
+                                child: Text('Allgemeine \nKontakte'),
+                                onPressed: () => setState(() {
+                                  _emergencyContacts =
+                                      createEmergencyContacts(
+                                          'GeneralEmergencyContacts');
+                                })))
+                      ]),
+                  _emergencyContacts
+                ])),
+      ),
     );
   }
+
+  static Column createEmergencyContacts(String buttonName) {
+    Column contactsColumn;
+    //TODO: make all contactButtons equal width
+    if (buttonName == 'MyEmergencyContacts') {
+      contactsColumn = Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+                width: double.infinity,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.phone),
+                      RaisedButton(
+                          color: defaultTheme.accentColor,
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                              BorderRadius.all(Radius.circular(18))),
+                          child: Text('Beispiel Notkontakt 1'),
+                          onPressed: () => launch("tel: 8383838"))
+                    ])),
+            Container(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.phone),
+                      RaisedButton(
+                          color: defaultTheme.accentColor,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(18))),
+                          child: Text('Beispiel Notkontakt 2'),
+                          onPressed: () => launch("tel: 123454321"))
+                    ]))
+          ]);
+    } else {
+      contactsColumn = Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+                width: double.infinity,
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.phone),
+                      RaisedButton(
+                          color: defaultTheme.accentColor,
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                              BorderRadius.all(Radius.circular(18))),
+                          child: Text('Beispiel Notruf'),
+                          onPressed: () => launch("tel: 8374858"))
+                    ])),
+            Container(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.phone),
+                      RaisedButton(
+                          color: defaultTheme.accentColor,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(18))),
+                          child: Text('Beispiel Rettungsdienst'),
+                          onPressed: () => launch("tel: 112221122"))
+                    ]))
+          ]);
+    }
+
+    return contactsColumn;
+  }
 }
+
